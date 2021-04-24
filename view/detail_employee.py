7776778ee70b26de86ec999261ba4911dcb5cbc6
HoDuyQuang7599 <hdquang.sgt@gmail.com>
2021-04-24 09:07:17 +0700

    #main detail employee

--- view/detail_employee.py
+++ view/detail_employee.py
@@ -1,6 +1,29 @@
 from tkinter import *
 
-class ClassName(object):
-    def __init__(self, *args):
-        super(ClassName, self).__init__(*args))
+class DetailEmployeeGUI(object):
+    def __init__(self, root, id):
+        self.root = root
+        self.root.title('Quản lý nhân viên')
+        self.root.geometry('1350x700+0+0')
+
+        #   Panel left
+        panel_left = Frame(self.root, bd = 4, relief = RIDGE, bg = 'gray')
+        panel_left.place(x = 20, y = 100, width = 400, height = 560)
+
+        layout_title = Label(panel_left,
+                            text='Chi tiết nhân viên',
+                            bg = 'gray',
+                            fg = 'black',
+                            compound = CENTER,
+                            font = ('time new roman', 24, 'bold'))
+        layout_title.grid(row = 0, columnspan = 2, pady = 50)
         
+        #   Load Avatar
+
+        #   Button back
+
+        #   Panel right
+        
+        #   Load Employee Detail Information
+
+        #   Button update
--- view/employee.py
+++ view/employee.py
@@ -1,7 +1,10 @@
 from tkinter import *
+from tkinter import ttk, filedialog
 from .registry import RegistryForm
 from .timekeeping import TimekeepingGUI
+from .detail_employee import DetailEmployeeGUI
 import os
+import pandas as pd
 
 class EmployeeGUI(object):
     def __init__(self, root):
@@ -9,7 +12,7 @@ class EmployeeGUI(object):
         self.root.title('Quản lý nhân viên')
         self.root.geometry('1350x700+0+0')
 
-         # Layouts menu
+        # Layouts menu
         Layout_Frame = Frame(self.root, bd=4, relief = RIDGE, bg = 'blue')
         Layout_Frame.place(x = 20, y = 100, width = 400, height = 560)
 
@@ -49,44 +52,117 @@ class EmployeeGUI(object):
         title = Label(self.root, text='Quản lý nhân viên', font=('time new roman',20,'bold'),bg='blue',fg='white')
         title.pack(side=TOP, fill=X)
 
+        #   Employee Screen
+        Employee_Frame = Frame(self.root, bd=4,bg = 'crimson', relief = RIDGE)
+        Employee_Frame.place(x = 500, y = 100, width = 840, height = 550)
+
+        #   Search
+        lbl_search = Label(Employee_Frame, text = 'Tìm kiếm nhân viên', bg = 'crimson', fg= 'white', font = ('time new roman', 14, 'bold'))
+        lbl_search.grid(row = 0, column = 0, padx = 20, pady = 10, sticky = 'w')
+
+        txt_search = Entry(Employee_Frame, width = 20, font = ('time new roman', 12), bd = 5, relief = GROOVE)
+        txt_search.grid(row = 0, column = 1, padx = 20, pady = 10)
+
+        btn_search = Button(Employee_Frame, text = 'Tìm kiếm', width = 10, pady = 5)
+        btn_search.grid(row = 0, column = 2, padx = 20, pady = 10)
+
         #   Button registry employee
-        btn_registry = Button(self.root,
+        btn_registry = Button(Employee_Frame,
             text = 'Thêm nhân viên',
             width = 250,
             bg = 'blue',
             compound = CENTER,
             font = ('time new roman', 14, 'bold'),
             command = self.registry)
-        btn_registry.place(x = 1050, y = 150, width = 250, height = 40)
+        btn_registry.place(x = 600, y = 100, width = 200, height = 30)
 
         #   Table data employee
-        Employee_Frame = Frame(self.root, bd=4, relief = RIDGE)
-        Employee_Frame.place(x = 550, y = 253, width = 750, height = 400)
+        table_frame = Frame(Employee_Frame, bd = 4, relief = RIDGE, bg = 'crimson')
+        table_frame.place(x = -2, y = 150, width = 830, height = 400)
 
-        header = ['STT', 'Họ và tên', 'Số ngày đi trễ', 'Số ngày nghỉ','Thao tác']
-        self.table(5,len(header),Employee_Frame,header)
+        scroll_x = Scrollbar(table_frame,orient = HORIZONTAL)
+        scroll_y = Scrollbar(table_frame,orient = VERTICAL)
 
-    def table(self,rows,columns,frame,header):
-        for row in range(rows):
-            for column in range(columns):
-                if (row == 0):
-                    lbl = Label(frame, text = header[column], bg = 'white', fg = 'black', padx = 3, pady = 3)
-                    lbl.config(font = ('time new roman',14))
-                    lbl.grid(row = row, column = column, sticky = 'nsew', padx = 1, pady = 1)
-                    frame.grid_columnconfigure(column, weight = 1)
-                else:
-                    lbl = Label(frame, text = 'row: ' + str(row) + ' column: ' + str(column), bg = 'black', fg = 'white', padx = 3, pady = 3)
-                    lbl.grid(row = row, column = column, sticky = 'nsew', padx = 1, pady = 1)
-                    frame.grid_columnconfigure(column, weight = 1)
+        table_employee = ttk.Treeview(
+            table_frame, 
+            columns = ('id', 'name', 'birth', 'email', 'late_date', 'off_date','action'),
+            xscrollcommand = scroll_x.set,
+            yscrollcommand = scroll_y.set
+        )
+        scroll_x.pack(side = BOTTOM, fill = X)
+        scroll_y.pack(side = RIGHT, fill = Y)
+        scroll_x.config(command = table_employee.xview)
+        scroll_y.config(command = table_employee.yview)
+
+        table_employee.heading('id', text = 'STT')
+        table_employee.heading('name', text = 'Họ và tên')
+        table_employee.heading('birth', text = 'Ngày sinh')
+        table_employee.heading('email', text = 'Email')
+        table_employee.heading('late_date', text = 'Số ngày đi trễ')
+        table_employee.heading('off_date', text = 'Số ngày nghỉ')
+        table_employee.heading('action', text = 'Thao tác')
+
+        table_employee['show'] = 'headings'
+
+        table_employee.column('id', width = 100)
+        table_employee.column('name', width = 150)
+        table_employee.column('birth', width = 100)
+        table_employee.column('email', width = 150)
+        table_employee.column('late_date', width = 100)
+        table_employee.column('off_date', width = 100)
+        table_employee.column('action', width = 100)
+
+        self.loadData(table_employee)
+        table_employee.pack(fill = BOTH, expand = 1)
+
+        #   Get data record
+        def getData():
+            selected = table_employee.focus()
+            return table_employee.item(selected, 'values')
+
+        #   Create binding click function
+        def clicker(e):
+            record = getData()
+            frame = Tk()
+            frame = DetailEmployeeGUI(frame,record[0])
+            self.root.destroy()
+
+
+        #   Bindings
+        table_employee.bind('<Double-1>', clicker)
+        
+
+    def loadData(self,employee_tree):
+        filename = os.path.abspath('data/Employee.xlsx')
+        df = pd.read_excel(filename)
+        #   Clear old record
+        # clearTree()
+
+        #   Loop column list
+        for column in employee_tree['column']:
+            employee_tree.heading(column, text = column)
+
+        #   Put data in treeview
+        df_rows = df.to_numpy().tolist()
+        for row in df_rows:
+            employee_tree.insert('','end',value=row)
+
+    def clearTree():
+        my_tree.delete(*my_tree.get_children())
 
     def registry(self):
         registry = Tk()
         registryForm = RegistryForm(registry)
         self.root.destroy()
 
-
-        
-
+class Employee(object):
+    def __init__(self):
+        self.id = ''
+        self.name = ''
+        self.birth = ''
+        self.email = ''
+        self.late_date = ''
+        self.off_date = ''
 
 
         
