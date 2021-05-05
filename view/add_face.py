a2e3b53097639e8043d63180e29f309d806158cb
Quang Trung <tqtrung.sgt@gmail.com>
2021-05-05 14:19:49 +0700

    add face to dataset

--- view/add_face.py
+++ view/add_face.py
@@ -1,10 +1,10 @@
 import PIL
-from PIL import Image,ImageTk
-import pytesseract
+from PIL import Image, ImageTk
 import cv2
 from tkinter import *
 import os
 import time
+import mediapipe as mp 
 
 class AddFaceGUI(object):
     def __init__(self, root, employee = None):
@@ -29,25 +29,73 @@ class AddFaceGUI(object):
         self.show_frame()
         self.root.mainloop()
 
+    def detect_face(self, frame):
+        mpFaceDetect = mp.solutions.face_detection
+        mpDraw = mp.solutions.drawing_utils
+
+        faceDetection = mpFaceDetect.FaceDetection(0.7)
+
+        results = faceDetection.process(frame)
+        boxx = []
+        if results.detections:
+            for id, detection in enumerate(results.detections):
+                # mpDraw.draw_detection(img, detection)
+                bboxC = detection.location_data.relative_bounding_box
+                ih, iw, ic = frame.shape
+                bbox = int(bboxC.xmin * iw), int(bboxC.ymin * ih), int(bboxC.width * iw), int(bboxC.height * ih)
+                boxx.append(bbox)
+        return boxx
+
     def show_frame(self):
         _, frame = self.cap.read()
-        frame = cv2.flip(frame, 1)
+
+        boxx = self.detect_face(frame)
+
+        for i in boxx:
+                                
+            self.fancyBox(frame, i)
+            # cv2.putText(frame, f'{int(detection.score[0] * 100)}%', (i[0], i[1] - 20), cv2.FONT_HERSHEY_PLAIN, 2
+            #         , (255, 0, 255), 2)
+                    
         cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
         img = PIL.Image.fromarray(cv2image)
         imgtk = ImageTk.PhotoImage(image=img)
+
         self.lmain.imgtk = imgtk
         self.lmain.configure(image=imgtk)
         self.lmain.after(10, self.show_frame)
 
+    def fancyBox(self, img, bbox, l=30, t=2, rt=1):
+        x, y, w, h = bbox
+        x1, y1 = x + w, y + h
+        # cv2.rectangle(img, bbox, (255, 0, 255), rt)
+        # Top left x, y
+        cv2.line(img, (x, y), (x + l, y), (255, 0, 255), t)
+        cv2.line(img, (x, y), (x, y + l), (255, 0, 255), t)
+        # Top right x, y
+        cv2.line(img, (x1, y), (x1 - l, y), (255, 0, 255), t)
+        cv2.line(img, (x1, y), (x1, y + l), (255, 0, 255), t)
+        # Bottom left x, y
+        cv2.line(img, (x, y1), (x + l, y1), (255, 0, 255), t)
+        cv2.line(img, (x, y1), (x, y1 - l), (255, 0, 255), t)
+        # Bottom right x, y
+        cv2.line(img, (x1, y1), (x1 - l, y1), (255, 0, 255), t)
+        cv2.line(img, (x1, y1), (x1, y1 - l), (255, 0, 255), t)
 
     def snapshot(self):
         user_id = self.employee[4]
         folder = os.path.abspath('data/face_train/'+user_id)
         # Get a frame from the video source
         ret, frame = self.get_frame()
-        name = "/frame-" + time.strftime("%d-%m-%Y-%H-%M-%S") + ".jpg"
-        if ret:
-            cv2.imwrite(folder + name, cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))
+        boxx = self.detect_face(frame)
+        for i in boxx:
+            x, y, w, h = i
+            roi_face = frame[y:y+h, x:x+w]   
+            roi_face = cv2.resize(roi_face, (180, 180))
+
+            name = "/frame-" + time.strftime("%d-%m-%Y-%H-%M-%S") + ".jpg"
+            if ret:
+                cv2.imwrite(folder + name, cv2.cvtColor(roi_face, cv2.COLOR_BGR2RGB))
 
     def get_frame(self):
         if self.cap.isOpened():
